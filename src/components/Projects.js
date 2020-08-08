d0b313f1d38f632ef6936c22dbdfe5d67d30ecf7
Nolan <nolan.boxill@neofinancial.com>
2020-08-08 00:46:21 -0600

    formatting

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,45 +1,56 @@
-   export const projects = [
-        {
-          title: "Bookswap",
-          image:
-            "https://user-images.githubusercontent.com/48977789/73147349-423e8900-4074-11ea-83dd-4291b6a44604.png",
-          hyperlink: "https://github.com/NolanDon/BookSwap",
-          description: "Full-Stack book trading app built with ruby on rails, googles maps and book API's",
-        },
-        {
-          title: "Schoodle",
-          image: "https://user-images.githubusercontent.com/48977789/69458027-a9b22280-0d2b-11ea-8c8c-79154d504f80.png",
-          hyperlink: "https://github.com/NolanDon/schoodle",
-          description: "Javascript App built with use of CDN and JQuery",
-        },
-        {
-          title: "Junglefy",
-          image: "https://user-images.githubusercontent.com/48977789/71332020-cb045b00-24f1-11ea-8ec6-d0b3001827e9.png",
-          hyperlink: "https://github.com/NolanDon/Rails-Project",
-          description: "Built with ruby on rails - E-commerce Website - Checkout enabled with stripe demo",
-        },
-        {
-          title: "Scheduler",
-          image: "https://user-images.githubusercontent.com/48977789/69908016-3731ea00-139e-11ea-9959-20ad66738a0d.png",
-          hyperlink: "https://github.com/NolanDon/Scheduler",
-          description: "Full stack ReactJS app for scheduling appointments"
-        },
-        {
-          title: "Static Portfolio Website",
-          image: "https://user-images.githubusercontent.com/48977789/89699114-1d0fba80-d8e2-11ea-87b8-7f4d2f74a9de.png",
-          hyperlink: "https://github.com/NolanDon",
-          description: "Built and deployed full-stack in just few hours with ruby on rails and basic html code",
-        },
-        {
-          title: "Tweeter",
-          image: "https://user-images.githubusercontent.com/48977789/70494126-5a0f7d00-1ac7-11ea-93f4-5d01947f3e12.png",
-          hyperlink: "https://github.com/NolanDon/tweeter",
-          description: "Built with JavaScript, JQuery, Ajax - Single-page Twitter clone",
-        },
-        {
-          title: "Tinyapp",
-          image: "https://user-images.githubusercontent.com/48977789/68065856-bcb97000-fcf4-11e9-95dc-b5032ef39b50.jpg",
-          hyperlink: "https://github.com/NolanDon/tinyapp",
-          description: "Built with Node and Express that allows users to shorten long URLs",
-        },
-      ]
+export const projects = [
+  {
+    title: "Bookswap",
+    image:
+      "https://user-images.githubusercontent.com/48977789/73147349-423e8900-4074-11ea-83dd-4291b6a44604.png",
+    hyperlink: "https://github.com/NolanDon/BookSwap",
+    description:
+      "Full-Stack book trading app built with ruby on rails, googles maps and book API's",
+  },
+  {
+    title: "Schoodle",
+    image:
+      "https://user-images.githubusercontent.com/48977789/69458027-a9b22280-0d2b-11ea-8c8c-79154d504f80.png",
+    hyperlink: "https://github.com/NolanDon/schoodle",
+    description: "Javascript App built with use of CDN and JQuery",
+  },
+  {
+    title: "Junglefy",
+    image:
+      "https://user-images.githubusercontent.com/48977789/71332020-cb045b00-24f1-11ea-8ec6-d0b3001827e9.png",
+    hyperlink: "https://github.com/NolanDon/Rails-Project",
+    description:
+      "Built with ruby on rails - E-commerce Website - Checkout enabled with stripe demo",
+  },
+  {
+    title: "Scheduler",
+    image:
+      "https://user-images.githubusercontent.com/48977789/69908016-3731ea00-139e-11ea-9959-20ad66738a0d.png",
+    hyperlink: "https://github.com/NolanDon/Scheduler",
+    description: "Full stack ReactJS app for scheduling appointments",
+  },
+  {
+    title: "Static Portfolio Website",
+    image:
+      "https://user-images.githubusercontent.com/48977789/89699114-1d0fba80-d8e2-11ea-87b8-7f4d2f74a9de.png",
+    hyperlink: "https://github.com/NolanDon",
+    description:
+      "Built and deployed full-stack in just few hours with ruby on rails and basic html code",
+  },
+  {
+    title: "Tweeter",
+    image:
+      "https://user-images.githubusercontent.com/48977789/70494126-5a0f7d00-1ac7-11ea-93f4-5d01947f3e12.png",
+    hyperlink: "https://github.com/NolanDon/tweeter",
+    description:
+      "Built with JavaScript, JQuery, Ajax - Single-page Twitter clone",
+  },
+  {
+    title: "Tinyapp",
+    image:
+      "https://user-images.githubusercontent.com/48977789/68065856-bcb97000-fcf4-11e9-95dc-b5032ef39b50.jpg",
+    hyperlink: "https://github.com/NolanDon/tinyapp",
+    description:
+      "Built with Node and Express that allows users to shorten long URLs",
+  },
+];
