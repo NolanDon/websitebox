ad10924028e80cba5cc6dd3932321a6679bfb03f
Nolan <nolan.boxill@neofinancial.com>
2020-08-07 20:27:07 -0600

    modularized more components including project card component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,93 +1,38 @@
 import React from "react";
 
-import { Box, Divider, Typography } from "@material-ui/core";
+import { makeStyles, Box, Divider, Typography } from "@material-ui/core";
 import PersistentSideBar from "./PersistentSideBar";
 import HomePageHeader from "./HomePageHeader";
-import { makeStyles } from "@material-ui/core/styles";
-// import ProjectCard from "./ProjectDisplayCard";
+// import { makeStyles } from "@material-ui/core/styles";
+import ProjectCard from "./ProjectDisplayCard";
 
 const useStyles = makeStyles({
   body: {
     display: "flex",
     justifyContent: "center",
+    // flexDirection: 'column'
   },
-  card: {
-    display: "flex",
-    justifyContent: "flex-start",
-    flexDirection: "column",
-    padding: "140px",
-
-  },
-  cardTitle: {
-    padding: '15px',
-    fontSize: '20px'
-  },
-  image: {
-    width: '680px',
-    height: '400px'
+  divider: {
+    width: '385px',
+    marginLeft: '300px'
   }
 });
 
 const HomePage = () => {
   const classes = useStyles();
 
-  const projects = [
-    {
-      title: "bookswap",
-      image: "https://user-images.githubusercontent.com/48977789/73147349-423e8900-4074-11ea-83dd-4291b6a44604.png",
-      hyperlink: "https://github.com/NolanDon/Junglefy",
-      description: "built with ruby on rails, book trading app",
-    }
-    // {
-    //   title: "title",
-    //   image: "image",
-    //   hyperlink: "hyperlink",
-    //   description: "description",
-    // },
-    // {
-    //   title: "title",
-    //   image: "image",
-    //   hyperlink: "hyperlink",
-    //   description: "description",
-    // },
-    // {
-    //   title: "title",
-    //   image: "image",
-    //   hyperlink: "hyperlink",
-    //   description: "description",
-    // },
-  ];
   return (
-      <Box>
-        <Box className={classes.body}>
-          <PersistentSideBar className={classes.sideBar} />
-          <HomePageHeader />
-          <Divider />
-          {/* <div className={classes.card}>
-        {ProjectCard(projects)}
-      </div> */}
-        </Box>
-      <div className={classes.card}>
-        {projects.map((item) => {
-          return (
-            <div className={classes.card}>
-              <Typography className={classes.cardTitle}>
-                {item.title}
-                </Typography>
-              <Typography>
-              {/* <img src={item.image} alt="Italian Trulli" className={classes.image} /> */}
-              </Typography>
-              <Typography>
-              <a href={item.hyperlink} target="_blank">
-                view source code
-              </a>
-              </Typography>
-              <Typography>{item.description}</Typography>
-            </div>
-          );
-        })}
-      </div>
+    <Box>
+      <Box className={classes.body}>
+        <PersistentSideBar />
+        <HomePageHeader />
+        <Typography>
+          Scroll down to view my Projects
+        </Typography>
       </Box>
+        <Divider className={classes.divider} />
+        <ProjectCard />
+    </Box>
   );
 };
 
