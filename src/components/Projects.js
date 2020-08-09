1d426262819f2c57e565032325b4f760ceff1a32
Nolan <nolan.boxill@neofinancial.com>
2020-08-09 01:06:02 -0600

    project description changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ export const projects = [
       "https://user-images.githubusercontent.com/48977789/73147349-423e8900-4074-11ea-83dd-4291b6a44604.png",
     hyperlink: "https://github.com/NolanDon/BookSwap",
     description:
-      "Ruby on Rails - full-stack app - Book trading app built using google maps & book API",
+      "Ruby on Rails - Full-Stack - Book trading app built using google maps & book API",
   },
   {
     title: "Schoodle",
@@ -20,14 +20,14 @@ export const projects = [
       "https://user-images.githubusercontent.com/48977789/71332020-cb045b00-24f1-11ea-8ec6-d0b3001827e9.png",
     hyperlink: "https://github.com/NolanDon/Rails-Project",
     description:
-      "Ruby on Rails - full-stack app - E-commerce Website - Checkout enabled with stripe demo",
+      "Ruby on Rails - Full-Stack - E-commerce Website - Checkout enabled with stripe demo",
   },
   {
     title: "Scheduler",
     image:
       "https://user-images.githubusercontent.com/48977789/69908016-3731ea00-139e-11ea-9959-20ad66738a0d.png",
     hyperlink: "https://github.com/NolanDon/Scheduler",
-    description: "ReactJS - full-stack app - appointment scheduler",
+    description: "ReactJS - Full-Stack - Appointment Scheduler",
   },
   {
     title: "Rails Website",
@@ -35,7 +35,7 @@ export const projects = [
       "https://user-images.githubusercontent.com/48977789/89699114-1d0fba80-d8e2-11ea-87b8-7f4d2f74a9de.png",
     hyperlink: "https://github.com/NolanDon",
     description:
-      "Ruby on Rails - static HTML5 portfolio website",
+      "Ruby on Rails - Static HTML5 portfolio website",
   },
   {
     title: "Tweeter",
@@ -51,6 +51,6 @@ export const projects = [
       "https://user-images.githubusercontent.com/48977789/68065856-bcb97000-fcf4-11e9-95dc-b5032ef39b50.jpg",
     hyperlink: "https://github.com/NolanDon/tinyapp",
     description:
-      "Node and Express CRUD app - Allows users to shorten long URLs",
+      "Node and Express app - Allows users to shorten long URLs",
   },
 ];
